a7fc13e645b14867f9c1fb35e3b142f614bf6ca7
farguello <f.arguello@intive.com>
2022-06-28 12:24:55 -0300

    first commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnCooldown = 1.5f;
+    private bool isOnCooldown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !isOnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isOnCooldown = true;
+        }
+
+        if (isOnCooldown)
+        {
+            spawnCooldown -= Time.deltaTime;
+            if (spawnCooldown <= 0)
+            {
+                isOnCooldown = false;
+                spawnCooldown = 1.5f;
+            }
+        }
+    }
+}
